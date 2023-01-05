db6316d244a84ce5e64352e59cccba74fb9b6f43
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-05 09:26:00 -0300

    criando o componente que irá gerenciar as tasks

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -1,15 +1,20 @@
 <template>
   <header>
     <h1>My To-Do List</h1>
+    <GerenciadorDeTasks/>
   </header>
 </template>
 
 <script lang="ts">
 
 import { defineComponent } from 'vue';
+import GerenciadorDeTasks from './GerenciadorDeTasks.vue';
 
 export default defineComponent({
   name: 'cabecalho-principal',
+  components: {
+    GerenciadorDeTasks
+  }
  
 });
 </script>
---
+++ src/components/GerenciadorDeTasks.vue
@@ -0,0 +1,19 @@
+<template>
+    <input type="text"/>
+    <button>Adicionar</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'GerenciadorDeTasks',
+
+})
+
+
+</script>
+
+<style>
+
+</style>

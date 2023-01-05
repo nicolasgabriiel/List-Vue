adee9f341f88969a6b993dbe048a8545ecc1c524
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-05 09:19:03 -0300

    Começando a estruturar o componente cabecalho

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Cabecalho/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Cabecalho from './components/Cabecalho.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Cabecalho
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url(./assets/css/reset.css);
 </style>
---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>My To-Do List</h1>
+  </header>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'cabecalho-principal',
+ 
+});
+</script>
+
+<style scoped>
+
+</style>

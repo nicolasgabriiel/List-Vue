faa47589be171db6c444a2e1bbde1cbf0cd8a0c6
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-30 21:10:22 -0300

    Fazendo tudo funcionar perfeitamente

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
  <header>
     <h1>My To-Do List</h1>
     <GerenciadorDeTasks @aoSalvarTarefa="salvarTarefa"/>
+    <Tarefas v-for="(tarefa, indice ) in tarefas" :key="indice" :tarefa="tarefa" />
   </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import GerenciadorDeTasks from './components/GerenciadorDeTasks.vue';
+import Tarefas from './components/Tarefas.vue';
 import ITarefa from './components/interfaces/ITarefa';
 
 export default defineComponent({
   name: 'App',
   components: {
-    GerenciadorDeTasks
+    GerenciadorDeTasks,
+    Tarefas
   },
   data(){
     return{
---
+++ src/components/Tarefas.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>{{tarefa.descricao}}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import ITarefa from './interfaces/ITarefa';
+
+export default defineComponent({
+    name:'Tarefas-Descricoes',
+    props:{
+        tarefa:{
+            type: Object as PropType<ITarefa>,
+            required: true
+        }
+    }
+})
+</script>
